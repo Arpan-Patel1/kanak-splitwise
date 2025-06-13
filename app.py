b6baf35da0ed781a840673b75c08f2c25031f020
Arpan-Patel1 <81413169+Arpan-Patel1@users.noreply.github.com>
2025-06-13 15:22:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -237,3 +237,21 @@ if st.session_state["generated_code"] is None:
     st.success("✅ Conversion completed!")
 else:
     st.success("✅ Already processed.")
+
+
+
+
+
+
+
+sqlite3.OperationalError: near "references": syntax error
+Traceback:
+File "C:\Users\arpapate\Desktop\Generate_macro_prompt\.venv\Lib\site-packages\streamlit\runtime\scriptrunner\exec_code.py", line 121, in exec_func_with_error_handling
+    result = func()
+             ^^^^^^
+File "C:\Users\arpapate\Desktop\Generate_macro_prompt\.venv\Lib\site-packages\streamlit\runtime\scriptrunner\script_runner.py", line 640, in code_to_exec
+    exec(code, module.__dict__)
+File "C:\Users\arpapate\Desktop\Generate_macro_prompt\test.py", line 28, in <module>
+    init_db()
+File "C:\Users\arpapate\Desktop\Generate_macro_prompt\test.py", line 19, in init_db
+    conn.execute("""
